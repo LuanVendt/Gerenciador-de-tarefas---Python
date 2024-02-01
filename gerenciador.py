94ddce662192757d9d99b04ff2b1128aa20cc3ff
Luan Vendt <luanvendt@gmail.com>
2024-02-01 11:21:19 -0300

    Funções do projeto - Visualizar tarefas

--- gerenciador.py
+++ gerenciador.py
@@ -4,11 +4,21 @@ import os
 def adicionar_tarefa(tarefas, nome_tarefa):
     # tarefa: nome da tarefa
     # completada: indicar se essa tarefa ja foi completada ou nao
-    tarefa = { 'tarefa': nome_tarefa, 'completada': False }
+    tarefa = { 'nome': nome_tarefa, 'completada': False }
     tarefas.append(tarefa)
     print(f"\nA tarefa '{nome_tarefa}' foi adicionada com sucesso!\n")
     input('Pressione qualquer tecla para voltar ao menu principal.')
     return
+
+def ver_tarefas(tarefas):
+    print('\nLista de tarefas:\n')
+
+    for indice, tarefa in enumerate(tarefas, start=1):
+        status = '✓' if tarefa['completada'] else ' '
+        nome_tarefa = tarefa['nome']
+        print(f'{indice}. [{status}] {nome_tarefa}')
+
+    input('\nPressione qualquer tecla para voltar ao menu principal.')
 
 tarefas = []
 
@@ -29,10 +39,12 @@ while True:
         nome_tarefa = input('\nDigite o nome da rarefa que deseja adicionar: ')
         adicionar_tarefa(tarefas, nome_tarefa)
 
+    elif escolha == 2:
+        os.system('cls')
+        ver_tarefas(tarefas)
     
-
     elif escolha == 6:
-        print('\nPrograma finalizado')
+        print('\nPrograma finalizado.')
         break
 
 
